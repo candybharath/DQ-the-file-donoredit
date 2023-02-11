e50f3c357325e4256b00a2209372ec98fd976a4d
Mayavi333 <105354161+Mayavi333@users.noreply.github.com>
2023-02-11 17:28:50 +0530

    Update ping.py

--- plugins/more/ping.py
+++ plugins/more/ping.py
@@ -4,7 +4,7 @@ Syntax: .ping"""
 import time
 import random
 from pyrogram import Client, filters
-from info import COMMAND_HAND_LER
+
 from plugins.helper_functions.cust_p_filters import f_onw_fliter
 
 # -- Constants -- #
@@ -12,12 +12,12 @@ ALIVE = "<b>ചത്തിട്ടില്ല മുത്തേ ഇവിട
 # -- Constants End -- #
 
 
-@Client.on_message(filters.command("alive", COMMAND_HAND_LER) & f_onw_fliter)
+@Client.on_message(filters.command("alive", "/") & f_onw_fliter)
 async def check_alive(_, message):
     await message.reply_text(ALIVE)
 
 
-@Client.on_message(filters.command("ping", COMMAND_HAND_LER) & f_onw_fliter)
+@Client.on_message(filters.command("ping", "/") & f_onw_fliter)
 async def ping(_, message):
     start_t = time.time()
     rm = await message.reply_text("...")
